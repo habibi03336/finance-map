273b4fec92a479020c750b8ff9bd6d6c49ccd669
하지훈 <jnh03336@gmail.com>
2024-05-14 21:39:00 +0900

    refactor: seperate loading indicator as common asset

--- src/app/heatmap/ui/design/components/fsheatmap.tsx
+++ src/app/heatmap/ui/design/components/fsheatmap.tsx
@@ -1,22 +1,5 @@
+import LoadingIndicator from "@/app/common/ui/asset/LoadingIndicator";
 import styled from "styled-components";
-
-const LoadingIndicator = () => {
-	return (
-		<>
-			<div className="absolute w-full rounded h-full bg-neutral absolute top-0 z-20 opacity-70 pointer-events-none"></div>
-			<div className="w-fit h-fit absolute top-0 left-0 bottom-0 right-0 m-auto z-30">
-				<div
-					className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-					role="status"
-				>
-					<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-						Loading...
-					</span>
-				</div>
-			</div>
-		</>
-	);
-};
 
 export default {
 	LoadingIndicator: styled(LoadingIndicator)``,
