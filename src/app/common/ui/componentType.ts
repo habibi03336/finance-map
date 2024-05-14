94ef0b0f763a05937c413cbd0970fa200feb6770
하지훈 <jnh03336@gmail.com>
2024-05-14 21:39:24 +0900

    feat: update design component(styled) type

--- src/app/common/ui/componentType.ts
+++ src/app/common/ui/componentType.ts
@@ -1,5 +1,5 @@
 import { StyledComponent } from "styled-components";
 
-export type DesignComponent = StyledComponent<any, any, {}, never>;
+export type DesignComponent = StyledComponent<any, any, {}, any>;
 export type DesignInputComponent = StyledComponent<"input", any, {}, never>;
 export type DesignSelectComponent = StyledComponent<"select", any, {}, never>;
